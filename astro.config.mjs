b4624836a6a81037081162b73d167599f2fdd5b6
neilmccracken-dev <neilmccracken.dev@gmail.com>
2026-07-13 22:00:01 -0700

    changed astro/netifly version to ^7.0.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,15 +10,15 @@ import keystatic from '@keystatic/astro';
 const isNetlify = process.env.NETLIFY;
 const isDev = process.env.NODE_ENV === 'development';
 export default defineConfig({
-  output: 'static',
-  // output: isNetlify ? 'server' : 'static',
+  // output: 'static',
+  output: isNetlify ? 'server' : 'static',
   site: isDev
     ? undefined
     : isNetlify
       ? undefined
       : 'https://neilmccracken-dev.github.io',
   base: isDev ? undefined : isNetlify ? undefined : '/astro-pt',
-  // adapter: isNetlify ? netlify() : undefined,
+  adapter: isNetlify ? netlify() : undefined,
 
   vite: {
     plugins: [tailwindcss()],
@@ -28,5 +28,5 @@ export default defineConfig({
     domains: ['images.unsplash.com', 'plus.unsplash.com'],
   },
 
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [react(), markdoc(), ...(isNetlify ? [keystatic()] : [])],
 });
