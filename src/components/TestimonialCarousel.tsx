a20b1aef21a769d2ab76f5762fa347f8a007ea8a
neilmccracken-dev <neilmccracken.dev@gmail.com>
2026-08-17 23:38:56 -0700

    First major step in redesign of home page[skip netlify]

--- src/components/TestimonialCarousel.tsx
+++ src/components/TestimonialCarousel.tsx
@@ -32,6 +32,7 @@ export default function TestimonialCarousel({
   }, [currentIndex, testimonials.length]);
 
   const handlePrev = (): void => {
+    console.log(currentIndex);
     setCurrentIndex((prev) =>
       prev === 0 ? testimonials.length - 1 : prev - 1,
     );
@@ -44,37 +45,40 @@ export default function TestimonialCarousel({
   const current = testimonials[currentIndex];
 
   return (
-    <div className="relative  mx-auto px-14 py-6   text-center">
-      {/* Left Arrow Button */}
-      <button
-        onClick={handlePrev}
-        className="absolute left-4 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center rounded-full border border-deep-teal text-deep-teal hover:deeper-teal hover:border-deeper-teal hover:bg-gray-50 transition-all cursor-pointer "
-        aria-label="Previous testimonial"
-      >
-        <ChevronLeft className="w-6 h-6" />
-      </button>
-      <div
-        key={currentIndex}
-        className="min-h-80 md:min-h-40 flex flex-col justify-center px-4 animate-fade-in"
-      >
-        {/* Testimonial Content */}
-        <div className="flex flex-col justify-center max-w-7xl mx-auto">
-          <p className="md:text-lg italic text-gray-700 mb-4">
-            "{current.review}"
-          </p>
-          <h4 className="font-bold text-gray-900">{current.name}</h4>
-          <span className="text-sm text-gray-500">{current.relationship}</span>
+    <div className="flex flex-col items-center justify-center md:min-h-72">
+      <div className="relative w-full text-center">
+        {/* Left Arrow Button */}
+        <button
+          onClick={handlePrev}
+          className="absolute z-10 left-4 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center rounded-full border border-deep-teal text-deep-teal  hover:border-deeper-teal hover:bg-gray-50 transition-all cursor-pointer "
+          aria-label="Previous testimonial"
+        >
+          <ChevronLeft className="w-6 h-6" />
+        </button>
+        <div
+          key={currentIndex}
+          className="min-h-80 md:min-h-40 flex flex-col justify-center px-4 animate-fade-in"
+        >
+          {/* Testimonial Content */}
+          <div className="flex flex-col justify-center max-w-84 md:max-w-2xl lg:max-w-6xl mx-auto">
+            <p className="md:text-lg italic leading-7 text-gray-700 mb-4">
+              "{current.review}"
+            </p>
+            <h4 className="font-bold text-gray-900">{current.name}</h4>
+            <span className="text-sm text-gray-500">
+              {current.relationship}
+            </span>
+          </div>
         </div>
+        {/* Right Arrow Button */}
+        <button
+          onClick={handleNext}
+          className="absolute right-4 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center rounded-full border border-deep-teal text-deep-teal  hover:border-deeper-teal cursor-pointer hover:bg-gray-700 transition-all"
+          aria-label="Next testimonial"
+        >
+          <ChevronRight className="w-6 h-6" />
+        </button>
       </div>
-
-      {/* Right Arrow Button */}
-      <button
-        onClick={handleNext}
-        className="absolute right-4 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center rounded-full border border-deep-teal text-deep-teal hover:deeper-teal hover:border-deeper-teal cursor-pointer hover:bg-gray-50 transition-all"
-        aria-label="Next testimonial"
-      >
-        <ChevronRight className="w-6 h-6" />
-      </button>
     </div>
   );
 }
--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -5,6 +5,7 @@ interface NavbarThemeProps {
   logo2?: ReactNode;
   logo3?: ReactNode;
   logo4?: ReactNode;
+  logo5?: ReactNode;
   children?: ReactNode;
 }
 
@@ -13,19 +14,22 @@ const NavbarThemeWrapper = ({
   logo2,
   logo3,
   logo4,
+  logo5,
   children,
 }: NavbarThemeProps) => {
-  const [theme, setTheme] = useState<'style1' | 'style2' | 'style3' | 'style4'>(
-    'style1',
-  );
+  const [theme, setTheme] = useState<
+    'style1' | 'style2' | 'style3' | 'style4' | 'style5'
+  >('style1');
   const themeConfig = {
-    style1: {
+    style1: { bg: 'bg-white', activeLogo: 'logo1' },
+
+    style2: {
       bg: 'bg-[#FFFFF0] bg-[radial-gradient(at_0%_0%,_hsla(39,47%,85%,1)_0,_transparent_50%),_radial-gradient(at_100%_100%,_hsla(39,47%,85%,0.6)_0,_transparent_50%)]',
-      activeLogo: 'logo1',
+      activeLogo: 'logo2',
     },
-    style2: { bg: 'bg-deeper-teal', activeLogo: 'logo2' },
-    style3: { bg: 'bg-[#7bc1d1]', activeLogo: 'logo3' },
-    style4: { bg: 'bg-ivory', activeLogo: 'logo4' },
+    style3: { bg: 'bg-deeper-teal', activeLogo: 'logo3' },
+    style4: { bg: 'bg-[#7bc1d1]', activeLogo: 'logo4' },
+    style5: { bg: 'bg-ivory', activeLogo: 'logo5' },
   };
   const currentTheme = themeConfig[theme] || themeConfig.style1;
   return (
@@ -43,7 +47,6 @@ const NavbarThemeWrapper = ({
             >
               {logo1}
             </div>
-
             <div
               className={
                 currentTheme.activeLogo === 'logo2' ? 'block' : 'hidden'
@@ -59,12 +62,20 @@ const NavbarThemeWrapper = ({
             >
               {logo3}
             </div>
+
             <div
               className={
                 currentTheme.activeLogo === 'logo4' ? 'block' : 'hidden'
               }
             >
               {logo4}
+            </div>
+            <div
+              className={
+                currentTheme.activeLogo === 'logo5' ? 'block' : 'hidden'
+              }
+            >
+              {logo5}
             </div>
           </a>
         </div>
@@ -77,10 +88,11 @@ const NavbarThemeWrapper = ({
             }
             className="p-1 border rounded bg-white text-black cursor-pointer"
           >
-            <option value="style1">Ivory Mesh</option>
-            <option value="style2">Deep Teal</option>
-            <option value="style3">Light Blue</option>
-            <option value="style4">Ivory</option>
+            <option value="style1">White</option>
+            <option value="style2">Ivory Mesh</option>
+            <option value="style3">Deep Teal</option>
+            <option value="style4">Light Blue</option>
+            <option value="style5">Ivory</option>
           </select>
         </div>
 
