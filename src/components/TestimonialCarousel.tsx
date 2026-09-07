b8883a60e64cd2f58efe1dfa4a113aaa5eca91eb
neilmccracken-dev <neilmccracken.dev@gmail.com>
2026-09-07 07:12:59 -0700

    updated about page outside pt section[skip netlify]

--- src/components/TestimonialCarousel.tsx
+++ src/components/TestimonialCarousel.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { ChevronLeft, ChevronRight } from 'lucide-react';
+import React, { useState, useEffect } from "react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 // Export the interface so Astro can reuse it for typing
 export interface Testimonial {
@@ -47,37 +47,110 @@ export default function TestimonialCarousel({
   return (
     <div className="flex flex-col items-center justify-center md:min-h-72">
       <div className="relative w-full text-center">
-        {/* Left Arrow Button */}
+        {/* Left Arrow */}
         <button
           onClick={handlePrev}
-          className="absolute z-10 left-4 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center rounded-full border border-deep-teal text-deep-teal  hover:border-deeper-teal hover:bg-gray-50 transition-all cursor-pointer "
+          className="
+        absolute z-10
+        hidden md:flex
+        left-1 top-1/2 -translate-y-1/2
+        w-10 h-10
+        items-center justify-center
+        rounded-full border border-deep-teal
+        text-deep-teal
+        hover:border-deeper-teal hover:bg-gray-50
+        transition-all cursor-pointer
+      "
           aria-label="Previous testimonial"
         >
           <ChevronLeft className="w-6 h-6" />
         </button>
+
+        {/* Testimonial */}
         <div
           key={currentIndex}
-          className="min-h-80 md:min-h-40 flex flex-col justify-center px-4 animate-fade-in"
+          className="
+        min-h-80 md:min-h-40
+        flex flex-col justify-center
+        px-5 md:px-14
+        animate-fade-in
+      "
         >
-          {/* Testimonial Content */}
-          <div className="flex flex-col justify-center max-w-84 md:max-w-2xl lg:max-w-6xl mx-auto">
-            <p className="md:text-lg italic leading-7 text-gray-700 mb-4">
+          <div
+            className="
+        flex flex-col justify-center
+        w-full
+        md:max-w-2xl
+        lg:max-w-6xl
+        mx-auto
+      "
+          >
+            <p
+              className="
+          text-sm md:text-lg
+          italic leading-7 md:leading-8
+          text-stone-900
+          mb-4
+        "
+            >
               "{current.review}"
             </p>
+
             <h4 className="font-bold text-gray-900">{current.name}</h4>
+
             <span className="text-sm text-gray-500">
               {current.relationship}
             </span>
           </div>
         </div>
-        {/* Right Arrow Button */}
+
+        {/* Right Arrow */}
         <button
           onClick={handleNext}
-          className="absolute right-4 top-1/2 -translate-y-1/2 w-10 h-10 flex items-center justify-center rounded-full border border-deep-teal text-deep-teal  hover:border-deeper-teal cursor-pointer hover:bg-gray-700 transition-all"
+          className="
+        absolute
+        hidden md:flex
+        right-1 top-1/2 -translate-y-1/2
+        w-10 h-10
+        items-center justify-center
+        rounded-full border border-deep-teal
+        text-deep-teal
+        hover:border-deeper-teal hover:bg-gray-50
+        transition-all cursor-pointer
+      "
           aria-label="Next testimonial"
         >
           <ChevronRight className="w-6 h-6" />
         </button>
+
+        {/* Mobile Navigation */}
+        <div className="flex justify-center gap-4 mt-4 md:hidden">
+          <button
+            onClick={handlePrev}
+            className="
+          flex w-10 h-10
+          items-center justify-center
+          rounded-full border border-deep-teal
+          text-deep-teal
+        "
+            aria-label="Previous testimonial"
+          >
+            <ChevronLeft className="w-6 h-6" />
+          </button>
+
+          <button
+            onClick={handleNext}
+            className="
+          flex w-10 h-10
+          items-center justify-center
+          rounded-full border border-deep-teal
+          text-deep-teal
+        "
+            aria-label="Next testimonial"
+          >
+            <ChevronRight className="w-6 h-6" />
+          </button>
+        </div>
       </div>
     </div>
   );
